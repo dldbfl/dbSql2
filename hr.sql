20c25d6acfa1526f3158ace4631830c8f92a7765
dldbfl <pangpang1861@naver.com>
2019-11-30 17:56:01 +0900

    2주차 주말과제

--- hr.sql
+++ hr.sql
@@ -1,2 +1,56 @@
-SELECT region_id, region_name, country_name
-FROM countries, regions;
+--join8 
+
+select * FROM countries;
+select * From regions;
+
+SELECT regions.region_id, region_name, country_name
+FROM regions, countries
+WHERE regions.region_id = countries.region_id 
+AND region_name IN ('Europe');
+    
+--join9
+select * from locations;
+
+SELECT regions.region_id, region_name, country_name, city
+FROM regions, countries, locations
+WHERE regions.region_id = countries.region_id
+AND countries.COUNTRY_ID = LOCATIONS.country_id 
+AND region_name IN ('Europe');
+    
+--join10  
+select * from DEPARTMENTS;
+
+SELECT regions.region_id, region_name, country_name, city, departments.DEPARTMENT_NAME
+FROM regions, countries, locations, departments
+WHERE regions.region_id = countries.region_id
+AND countries.COUNTRY_ID = LOCATIONS.country_id 
+AND locations.location_id = departments.location_id
+AND region_name IN ('Europe');
+    
+--join11
+select * from employees;
+
+SELECT regions.region_id, region_name, country_name, city, departments.DEPARTMENT_NAME, (employees.FIRST_NAME||employees.LAST_NAME) name
+FROM regions, countries, locations, departments,employees
+WHERE regions.region_id = countries.region_id
+AND countries.COUNTRY_ID = LOCATIONS.country_id 
+AND locations.location_id = departments.location_id
+AND departments.DEPARTMENT_ID = employees.DEPARTMENT_ID
+AND region_name IN ('Europe');
+
+--join12
+select * from jobs;
+
+SELECT employee_id,(employees.first_name||employees.last_name) name, jobs.job_id, job_title
+FROM employees, jobs
+WHERE employees.job_id = jobs.job_id;
+
+--join13
+select * from employees;
+select * from jobs; 
+
+SELECT m.manager_id mng_id,(e.first_name||e.last_name) mgr_name ,m.employee_id,(m.first_name||m.last_name) name, jobs.job_id, job_title
+FROM employees e, employees m,jobs
+WHERE m.job_id = jobs.job_id
+AND m.manager_id = e.employee_id
+ORDER BY mng_id;
